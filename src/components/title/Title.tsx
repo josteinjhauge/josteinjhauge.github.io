8a4b90a13883ca36f3cac5d6bbd0d179cdf1a6cc
jostein.hauge <jostein.hauge@bouvet.no>
2023-03-02 09:18:38 +0100

    title

--- src/components/title/Title.tsx
+++ src/components/title/Title.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from "./Title.module.css";
-import { ReactComponent as Logo } from "../../assets/react.svg";
+import react from "../../assets/react.svg";
 
 const Title = () => {
   return (
     <div className={styles.container}>
-      <Logo />
+      <img src={react} />
       <h3 className={styles.title}>WorkingTitle</h3>
     </div>
   );
